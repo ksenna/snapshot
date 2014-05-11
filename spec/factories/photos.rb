93ef5ce08b7ae304133ba3b4bdfdff7e49d534df
Kelly <jkellysenna@gmail.com>
2014-05-11 15:41:37 -0400

    Factory Girl factories

---
+++ spec/factories/photos.rb
@@ -0,0 +1,8 @@
+
+
+FactoryGirl.define do
+	factory :photo do |f|
+		f.name "Test Photo"
+		f.description "Test Photo's Description"
+	end
+end
---
+++ spec/models/photo_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper' 
+
+describe Photo do 
+	it "has a name" 
+	it "has a description"
+	it "is invalid without a name" 
+end
